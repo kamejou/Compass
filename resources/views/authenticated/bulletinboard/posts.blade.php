03848e66296cedf38c408f6a573a72162ae942e0
DESKTOP-HC1GNB8\81803 <takumi.kame032019@gmail.com>
2024-06-20 21:09:19 +0900

    余計なものは言ってたので削除のため更新

--- resources/views/authenticated/bulletinboard/posts.blade.php
+++ resources/views/authenticated/bulletinboard/posts.blade.php
@@ -17,7 +17,7 @@
             @if(Auth::user()->is_Like($post->id))
             <p class="m-0"><i class="fas fa-heart un_like_btn" post_id="{{ $post->id }}"></i><span class="like_counts{{ $post->id }}"></span></p>
             @else
-            <p class="m-0"><i class="fas fa-heart like_btn" post_id="{{ $post->id }}"></i><span class="like_counts{{ $post->id }}"></span> {{ $post->lik_post_id->count() }} </p>
+            <p class="m-0"><i class="fas fa-heart like_btn" post_id="{{ $post->id }}"></i><span class="like_counts{{ $post->id }}"></span></p>
             @endif
           </div>
         </div>
